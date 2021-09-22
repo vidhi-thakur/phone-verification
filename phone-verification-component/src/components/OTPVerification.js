2b3192e8c8bd8ceacbf8bdfc038cd291b3b1a140
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-09-22 20:43:36 +0530

    FEAT: UPDATE FOCUS ON USER INPUT
    -once a digit is entered, the focus moves to the next input
    -backspace deletes the input and moves the focus on previous input

--- phone-verification-component/src/components/OTPVerification.js
+++ phone-verification-component/src/components/OTPVerification.js
@@ -1,18 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './OTPVerification.css'
 
 function OTPVerification() {
+
+    const initialState = {
+        input01: "",
+        input02: "",
+        input03: "",
+        input04: "",
+        input05: "",
+        input06: ""
+    }
+
+    const [foucsState, setFocusState] = useState(1)
+    const [inputForm, setInputForm] = useState(initialState)
+
+    useEffect(() => {
+        setFocusState(foucsState+1)
+    }, [inputForm])
+
+    // function updateAutoFocus() {
+    //     if()
+    // }
+
+    function validInput(input) {
+        if(Number(input)>=0 && Number(input)<=9) return true
+        else return false
+    }
+
+    const moveFocus = (e) => {
+        var target = e.target;
+        var inputMaxLength = target.maxLength
+        var inputLength = target.value.length
+        if(inputLength>=inputMaxLength) {
+            var next = target;
+            while(next = next.nextElementSibling) {
+                if(next === null) break;
+                if(target.tagName.toLowerCase() === "input") {
+                    next.focus();
+                    break;
+                }
+            }
+        }
+        else if(inputLength === 0) {
+            var prev = target;
+            while(prev = prev.previousElementSibling) {
+                if(prev === null) break;
+                if(target.tagName.toLowerCase() === "input") {
+                    prev.focus();
+                    break;
+                }
+            }
+        }
+    }
+
     return (
         <div className="OTPVerification">
             <header>Phone Verification</header>
             <form>
                 <label>Enter the OTP you received on 89206 - 6XXXX</label>
-                <input type="number" min={0} max={9} maxLength={1} autoFocus={false} required />
-                <input type="number" min={0} max={9} maxLength={1} autoFocus={false} required />
-                <input type="number" min={0} max={9} maxLength={1} autoFocus={false} required />
-                <input type="number" min={0} max={9} maxLength={1} autoFocus={false} required />
-                <input type="number" min={0} max={9} maxLength={1} autoFocus={false} required />
-                <input type="number" min={0} max={9} maxLength={1} autoFocus={false} required />
+                <div onKeyUp={(e) => moveFocus(e)}>
+                    <input onChange={(e)=>setInputForm({...inputForm, input01: e.target})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input02: e.target})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input03: e.target})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input04: e.target})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input05: e.target})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input06: e.target})} className="input" type="text" maxLength={1} required />
+                </div>
                 <button>Verify Phone Number</button>
             </form>
         </div>
