15ddd20f3cfc64e4f5f6a0cdcbe54f9eb60e7e6e
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-09-22 17:41:12 +0530

    ADDED BUTTON
    -added button to update verifivation state
    -added style to the button

--- phone-verification-component/src/App.js
+++ phone-verification-component/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { useState } from 'react'
+import OTPVerification from './components/OTPVerification';
 
 function App() {
+  const [verifyOTP, setVerifyOTP] = useState(false)
   return (
     <div className="App">
-      
+      <div className="main">
+        {
+          !verifyOTP? <button onClick={() => setVerifyOTP(true)} className="OTPVerificationButton">Verify OTP</button>: <OTPVerification />
+        }
+      </div>
     </div>
   );
 }
