05186709ca22102f8cd37bdb5805cc53f7b8a0a4
Vidhi Thakur <thakurvidhi997@gmail.com>
2021-09-23 00:05:26 +0530

    FEAT: CHECK VALID INPUT

--- phone-verification-component/src/components/OTPVerification.js
+++ phone-verification-component/src/components/OTPVerification.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './OTPVerification.css'
 
 function OTPVerification() {
@@ -11,17 +11,8 @@ function OTPVerification() {
         input05: "",
         input06: ""
     }
-
-    const [foucsState, setFocusState] = useState(1)
     const [inputForm, setInputForm] = useState(initialState)
-
-    useEffect(() => {
-        setFocusState(foucsState+1)
-    }, [inputForm])
-
-    // function updateAutoFocus() {
-    //     if()
-    // }
+    const [error, setError] = useState("")
 
     function validInput(input) {
         if(Number(input)>=0 && Number(input)<=9) return true
@@ -32,43 +23,56 @@ function OTPVerification() {
         var target = e.target;
         var inputMaxLength = target.maxLength
         var inputLength = target.value.length
-        if(inputLength>=inputMaxLength) {
-            var next = target;
-            while(next = next.nextElementSibling) {
-                if(next === null) break;
-                if(target.tagName.toLowerCase() === "input") {
-                    next.focus();
-                    break;
+        if(!validInput(target.value)) {
+            setError("Enter the valid input.")
+        } else {
+            setError("")
+            if(inputLength>=inputMaxLength) {
+                setError("")
+                var next = target;
+                while(next = next.nextElementSibling) {
+                    if(next === null) break;
+                    if(target.tagName.toLowerCase() === "input") {
+                        next.focus();
+                        break;
+                    }
+                }
+            }
+            else if(inputLength === 0) {
+                var prev = target;
+                while(prev = prev.previousElementSibling) {
+                    if(prev === null) break;
+                    if(target.tagName.toLowerCase() === "input") {
+                        prev.focus();
+                        break;
+                    }
                 }
             }
         }
-        else if(inputLength === 0) {
-            var prev = target;
-            while(prev = prev.previousElementSibling) {
-                if(prev === null) break;
-                if(target.tagName.toLowerCase() === "input") {
-                    prev.focus();
-                    break;
-                }
-            }
-        }
+        
     }
+    
 
     return (
         <div className="OTPVerification">
             <header>Phone Verification</header>
             <form>
-                <label>Enter the OTP you received on 89206 - 6XXXX</label>
+                <label>Enter the OTP you received on 89206-6XXXX</label>
                 <div onKeyUp={(e) => moveFocus(e)}>
-                    <input onChange={(e)=>setInputForm({...inputForm, input01: e.target})} className="input" type="text" maxLength={1} required />
-                    <input onChange={(e)=>setInputForm({...inputForm, input02: e.target})} className="input" type="text" maxLength={1} required />
-                    <input onChange={(e)=>setInputForm({...inputForm, input03: e.target})} className="input" type="text" maxLength={1} required />
-                    <input onChange={(e)=>setInputForm({...inputForm, input04: e.target})} className="input" type="text" maxLength={1} required />
-                    <input onChange={(e)=>setInputForm({...inputForm, input05: e.target})} className="input" type="text" maxLength={1} required />
-                    <input onChange={(e)=>setInputForm({...inputForm, input06: e.target})} className="input" type="text" maxLength={1} required />
+                    <input autoFocus onChange={(e)=>setInputForm({...inputForm, input01: validInput(e.target.value) ? e.target.value: ""})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input02: validInput(e.target.value) ? e.target.value: ""})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input03: validInput(e.target.value) ? e.target.value: ""})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input04: validInput(e.target.value) ? e.target.value: ""})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input05: validInput(e.target.value) ? e.target.value: ""})} className="input" type="text" maxLength={1} required />
+                    <input onChange={(e)=>setInputForm({...inputForm, input06: validInput(e.target.value) ? e.target.value: ""})} className="input" type="text" maxLength={1} required />
                 </div>
-                <button>Verify Phone Number</button>
+                <div className="editInfo">
+                    <span className="text-blue">Change number</span>
+                    <span className="text-blue">Re-send OTP</span>
+                </div>
+                <button className="button OTPSubmitButton">Verify Phone Number</button>
             </form>
+            {error && <small className="error">{error}</small>}
         </div>
     )
 }
